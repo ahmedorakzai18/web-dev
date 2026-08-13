476c5b269389aad2a784aaf83b7e8fa38666ae8d
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-13 23:03:58 +0500

    javascript started

---
+++ javascript/day-1/hello.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+console.log(a = 3) 
+console.log(b = 4) 
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
