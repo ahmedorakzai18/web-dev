3c416f8de311eca7fbb38155b7a326999608b1f5
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-14 20:48:35 +0500

    practice set 3

---
+++ javascript/day-2/practice-set/003practice.js
@@ -0,0 +1,31 @@
+// Question: 1
+let x = "Code one Returns: "; 
+console.log(x); 
+let str = "aso"; 
+
+if(str.length > 3 && str[0] == "a")
+{
+    console.log("its a Good String");
+}
+else
+{
+    console.log("Not a Good String");
+}
+
+
+// Question: 2
+
+
+let y = "\n\nCode two Returns: "; 
+console.log(y); 
+
+let num = 12; 
+
+if(num%3 == 0  && (num+1 === 15 || num - 1 === 11))
+{
+    console.log("Safe");
+}
+else 
+{
+    console.log("Not Safe");
+}
