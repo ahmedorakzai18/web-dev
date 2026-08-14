5ea3f99a5a39b0d2831fb944a84c45427fa6bdc4
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-14 22:28:55 +0500

    Prompt-Box

--- javascript/day-2/09alert.js
+++ javascript/day-2/09alert.js
@@ -1,2 +1,6 @@
 alert("a pop-up to make u stress and u have to click ok.");
 
+console.error("this is an error"); 
+
+console.warn("this is a warning msg"); 
+
---
+++ javascript/day-2/10prompt.js
@@ -0,0 +1,9 @@
+// 1. Prompt and log the first name
+let firstName = prompt("Enter first name: ");
+console.log(firstName); 
+
+// 2. Prompt and log the last name
+let lastName = prompt("Enter last name: ");
+console.log(lastName); 
+
+console.log(`${firstName} ${lastName}`);
