2f1611a0235a77b7df0ca07f6060a5eb4e5ea530
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-14 18:03:56 +0500

    Nested-If-Else & Logical Operators

---
+++ javascript/day-2/06nested-if-else.js
@@ -0,0 +1,18 @@
+const marks = 80; 
+
+if(marks >= 33)
+{
+    if(marks>=80 && marks < 100)
+    console.log("Passed with Great Marks.");
+
+    else if(marks > 100)
+    console.log("Invalid Number");
+
+    else
+    console.log("Passed");
+}
+
+else
+{
+    console.log("Better luck Next time!");
+}
