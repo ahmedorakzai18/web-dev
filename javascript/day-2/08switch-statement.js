3468885b3a06db696072e71edf4affdbc2b65ffb
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-14 21:18:35 +0500

    Switch-Statement File

---
+++ javascript/day-2/08switch-statement.js
@@ -0,0 +1,21 @@
+// Example of Traffic Lights
+
+let color = "red"; 
+
+switch(color)
+{
+    case "red": 
+    console.log("STOP"); 
+    // break;
+
+        case "yellow": 
+    console.log("WAIT"); 
+    // break;
+
+        case "green": 
+    console.log("GOOOOOOOOOOO"); 
+    // break;
+
+    default:
+    console.log("NOT THE VALID COLOR.");
+}
