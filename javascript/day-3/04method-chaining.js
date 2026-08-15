5cd8aac5d60e4e4fc75791cf4ec1f3a59629331f
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-15 12:26:40 +0500

    method-chaining

---
+++ javascript/day-3/04method-chaining.js
@@ -0,0 +1,9 @@
+console.log("So the bellow string is in lower-case and also need some trimming.");
+
+let a = "   lkjds f lksj f      "; 
+
+const b = a.trim().toUpperCase(); 
+
+console.log(b);
+
+console.log("\nNow it is trimmed as well as in Upper-case.");
