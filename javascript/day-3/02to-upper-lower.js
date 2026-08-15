fdcfe1fcc780359052fe335cd74c888ecef431e3
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-15 11:46:52 +0500

    To lower/Upper-Case Method

---
+++ javascript/day-3/02to-upper-lower.js
@@ -0,0 +1,13 @@
+let msg = "i am written all in lower-case."
+
+console.log(msg);
+const UC = msg.toUpperCase(msg);
+
+console.log("Now after the Upper Case method i am In Upper.\n" , UC); 
+
+let gsm = "\nI AM WRITTEN ALL IN UPPER-CASE."
+
+console.log(gsm);
+const LC = gsm.toLowerCase(gsm);
+
+console.log("Now after the lower Case method i am In Lower.\n" , LC); 
