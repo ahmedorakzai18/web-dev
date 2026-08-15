80594ebc2b032aeb9f66ff8c1a27d3196cde45ff
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-15 11:32:51 +0500

    Trim-Method

---
+++ javascript/day-3/01trim.js
@@ -0,0 +1,13 @@
+// let msg = "     Hello       there i am learning Javascript!!!     ";
+
+// msg = msg.trim(); 
+
+// console.log(msg);
+
+// Code: 2
+
+let password = prompt("Enter your Password");
+
+const newpass = password.trim(); 
+
+console.log(newpass);
