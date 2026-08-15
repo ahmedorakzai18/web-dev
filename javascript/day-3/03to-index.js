0970eddb06cb720c5f21085e8c446d56786a088c
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-15 12:19:30 +0500

    Index-of file

---
+++ javascript/day-3/03to-index.js
@@ -0,0 +1,13 @@
+let x = "iloveCoding"; 
+
+console.log(x);
+
+const q = x.indexOf("e");
+const w = x.indexOf("Coding");
+const e = x.indexOf("o");
+const r = x.indexOf("z");
+
+console.log(q);
+console.log(w);
+console.log(e);
+console.log(r);
