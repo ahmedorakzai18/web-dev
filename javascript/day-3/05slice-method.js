07d6ee0e995c10127dce2cb182a26bc3bdc4a2e3
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-16 10:23:49 +0500

    Slice-Method

---
+++ javascript/day-3/05slice-method.js
@@ -0,0 +1,8 @@
+console.log("So the bellow We have a String");
+
+let a = "Hello I am learning Javascript"; 
+console.log(a);
+const b = a.slice(6 , 15);
+console.log(b);
+
+console.log("\nNow it is GIVEING THE SLICED VERSION.");
