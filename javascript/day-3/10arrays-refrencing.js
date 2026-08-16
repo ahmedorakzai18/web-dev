7894a56f6e93e78689d30a54424ba6d356ddcf03
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-16 18:08:52 +0500

    Arrays Refrencing

---
+++ javascript/day-3/10arrays-refrencing.js
@@ -0,0 +1,16 @@
+let arr1 = [10, 20, 30];
+
+let arr2 = arr1;
+
+console.log(arr1);
+console.log(arr2);
+
+
+let arr = ['a' , 'b' , 'c']; 
+
+let arrcopy = arr ;
+
+arrcopy.push('d'); 
+
+console.log(arrcopy);       // Changed in one means change in the other as well
+console.log(arr); 
