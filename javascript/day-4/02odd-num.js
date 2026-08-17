59d2b00de9ee0be4fde371101de5360315e2f0ae
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-17 17:53:30 +0500

    Even/Odd Loops

---
+++ javascript/day-4/02odd-num.js
@@ -0,0 +1,20 @@
+for(let i = 1; i<=10; i++)
+{
+    if(i%2 != 0)
+    console.log(i)
+}
+
+// OR
+console.log("\nOdd with another method\n");
+for(let x = 11; x<=21; x=x+2)
+{
+       console.log(x); 
+}
+
+// SAME FOR EVEN
+console.log("\nEVEN IN REVERSE\n");
+for(let y = 10; y>=1; y--)
+{
+    if(y%2 == 0)
+    console.log(y);
+}
