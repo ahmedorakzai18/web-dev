d79cca0151689105b5256e47c4a41216af2ed2ab
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-17 21:10:53 +0500

    while-loops

---
+++ javascript/day-4/05while-loops.js
@@ -0,0 +1,19 @@
+let x = 1; 
+
+while(x <= 10)
+{
+    console.log(x);
+    x++;
+}
+
+console.log("");
+console.log("Reverse");
+console.log("");
+// Reverse
+let i = 10; 
+
+while(i>=1)
+{
+    console.log(i);
+    i--;
+}
