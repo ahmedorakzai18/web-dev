65bb424f0899a666bd88c1295138ad13a089e8e6
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-17 10:49:03 +0500

    Nested-Arrays

---
+++ javascript/day-3/11nested-arrays.js
@@ -0,0 +1,17 @@
+let num = [[2,3,42] , [32,52,72] , [73,234,10]]; 
+
+console.log(num[2]);    // Acces the 2nd Index array inside the main array.
+
+console.log(num[2][2]);     // Acces the 2nd element inside the second array.
+
+
+// Example 2:
+
+let students = [
+    ["Ahmed", 20, "Software Engineering"],
+    ["Ali", 21, "Computer Science"],
+    ["Usman", 19, "Information Technology"]
+];
+
+console.log(students[0][1]);
+console.log(students[1][2]);
