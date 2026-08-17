da87f20a277e84805c5df0c12458823d8f8e9b6b
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-17 18:15:10 +0500

    infinite loop

---
+++ javascript/day-4/03infinite.js
@@ -0,0 +1,13 @@
+for(let i = 1; i<=10; i++)
+{
+    console.log(`${5} X ${i} = ${5*i}`);
+}
+
+// ASK USER
+
+let n = prompt("Enter the Number you want the table of till 10: ");
+
+for(let i = 1; i<=10 ; i++)
+{
+    console.log(n*i);
+}
