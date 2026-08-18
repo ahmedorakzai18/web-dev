43bc397e63804a325d07de8c12808c0784f272ff
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-18 22:21:45 +0500

    nested Objects

---
+++ javascript/day-5/05nested-objects.js
@@ -0,0 +1,20 @@
+let studentInfo = {
+    s1 : {
+        name: "ali",
+        roll: "25"
+    } ,
+    s2 : {
+        name: "sajid",
+        roll: "73"
+    },
+    s3 : {
+        name: "Talha",
+        roll: 29
+    }
+}
+
+console.log(studentInfo);
+
+console.log("-------------------");
+
+console.log(studentInfo.s1);
