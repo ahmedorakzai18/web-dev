bc81323293c5410b41754fea8789a61c7049d05a
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-18 18:44:41 +0500

    Object Litrals

---
+++ javascript/day-5/01object-literals.js
@@ -0,0 +1,17 @@
+const student = {Name:"Ali" , Roll:23 , Grade:12};
+
+console.log("Name:" , student.Name);
+console.log("Roll-No:" , student.Roll);
+console.log("Grade:" , student.Grade);
+
+const items = {
+    quantity : 15,
+    price : 2910,
+    colors : ["red" , "green" , "blue"]
+}
+
+console.log("---------------------");
+
+console.log("Price:" , items.price);
+console.log("Quantity:" , items.quantity);
+console.log("Colors:" , items.colors);
