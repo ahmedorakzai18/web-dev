f3f89b4837890b28bb91fa8b408416cad4f12e91
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-18 11:09:04 +0500

    nest for-of Loops

---
+++ javascript/day-4/11nested-for-of-loops.js
@@ -0,0 +1,8 @@
+let animals = [["Ape" , "Ant" , "alligator"] , ["bull" , "bat"  , "bear"] , ["cat" , "cow" , "camel"] , ["dog" , "donkey" , "duck"] ];
+
+for(list of animals)
+{
+for(animal of list){
+        console.log(animal)}
+    
+}
