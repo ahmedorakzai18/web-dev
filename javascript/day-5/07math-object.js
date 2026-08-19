a9b18c970a14dea59484badb3d2ddc0a111a69dd
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-19 12:17:18 +0500

    math object

---
+++ javascript/day-5/07math-object.js
@@ -0,0 +1,15 @@
+console.log(Math.PI);   // value of pi
+
+console.log(Math.pow(2,2));     // power
+
+console.log(Math.floor(324.8));     // gives round-off value
+
+console.log(Math.abs(-324));     // gives absolute
+
+console.log(Math.ceil(29.8));    // round-off to bigger number
+
+console.log(Math.random());     // gives a random value (decimal of zero)
+
+let x = Math.random();
+
+console.log(x);
