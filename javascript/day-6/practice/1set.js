ff259a889be9864df4c33616e8458b50cc575571
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-19 17:04:40 +0500

    Practice Updated

--- javascript/day-6/practice/1set.js
+++ javascript/day-6/practice/1set.js
@@ -12,7 +12,7 @@ poem();
 // Create a Function to roll a dice & always display the value of the dice (1 to 6).
 
 
-console.log("---------------Dice-Roll---------------");
+console.log("\n\n---------------Dice-Roll---------------");
 function diceRoll()
 {
 let dice = Math.floor(Math.random()*6 +1);
@@ -21,3 +21,64 @@ console.log(dice);
 }
 
 diceRoll();
+
+
+// Create a Function that gives us the average of 3 numbers.
+console.log("\n\n---------------Average of 3 Numbers---------------");
+
+function average(a , b , c)
+{
+    return (a+b+c)/3;
+}
+
+console.log(average(2,3,4));
+
+
+// Create a Function that prints the multiplication table of a number.
+
+function multiply(no)
+{
+    for(let i = 1; i<=10; i++)
+        {
+            console.log(`${no} X ${i} = ${no*i}`);
+        }    
+}
+
+multiply(4);
+
+
+// Create a Function that returns the sum of numbers from 1 to n.
+
+console.log("\n\n---------------SUM of 'n' Numbers---------------");
+
+function sumAll(n)
+{
+    let sum = 0;
+
+    for(let i = 1; i<=n; i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+console.log(sumAll(5));
+
+
+// Create a Function that returns the concatenation of all strings in an array.
+
+console.log("\n\n---------------Concatenation of all Strings---------------");
+
+let str = ["Hello" , " World" , " and" , " all"];
+
+function concat(str)
+{
+    let combine = "";
+    for(let i = 0; i < str.length; i++)
+    {
+        combine += str[i];
+    }
+    return combine;
+}
+
+console.log(concat(str)); 
