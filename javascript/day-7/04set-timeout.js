437b35025613e482978ccd1a42440d4a333d088c
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-20 21:26:00 +0500

    setTimer

---
+++ javascript/day-7/04set-timeout.js
@@ -0,0 +1,13 @@
+setTimeout(function()
+{
+    console.log("hello World");
+},3000);
+
+setTimeout(() => 
+{
+    console.log("Hello Galaxy");
+} , 2000);
+
+setTimeout((a , b) =>{
+    console.log(1+32);
+}, 1500);
