256de3213f3e7bb8e4efd21ec10538e699458876
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-20 11:43:20 +0500

    try-catch-finally Statements

---
+++ javascript/day-7/02try-and-catch.js
@@ -0,0 +1,24 @@
+//"Try this code. If something goes wrong, catch the error and handle it."
+
+console.log("Hello1");
+console.log("Hello2");
+console.log("Hello3");
+console.log("Hello4");
+
+console.log("");
+
+// let a  = 23;
+try{
+console.log(a);}
+catch(e){
+    console.log("\nThere is some kind of error in the code here")
+    console.log(e);
+}
+finally{
+    console.log("this runs anyway, for the love of the game. CUZ IT IS IN 'finally'. Even if there is no error this statement runs");
+}
+
+console.log("\nhello5");
+console.log("hello6");
+console.log("hello7");
+console.log("hello8");
