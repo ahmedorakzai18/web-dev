0b0e8998f797aca96d29401c96075621b0cce22d
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-21 22:56:49 +0500

    filter-method

---
+++ javascript/day-8/02filter-method.js
@@ -0,0 +1,16 @@
+let numbers = [32,2,521,12,20,42,11,10];
+
+let result = numbers.filter(number => {
+    return number > 20;
+});
+
+console.log(result);
+
+
+let numberss = [1,2,3,4,5,6,7,8,9,10];
+
+let resultt = numberss.filter(number => {
+    return number%2 == 0;
+});
+
+console.log(resultt);
