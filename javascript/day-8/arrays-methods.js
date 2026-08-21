1452e7b5c52554cff2a56132f7d6d9e354b71fd2
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-21 22:18:22 +0500

    array-methods (forEach)

---
+++ javascript/day-8/arrays-methods.js
@@ -0,0 +1,17 @@
+let arr = ["afjd" , "jlk" , "riuoi"];
+
+let print = function(ele){
+console.log(ele);
+}; 
+
+// arr.forEach(print);
+
+// arr.forEach(function(ele){
+//     console.log(ele);
+// });
+
+// or
+
+// arr.forEach((ele) => {
+//     console.log(ele);
+// });
