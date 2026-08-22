2677dbdbe9bd8ba08295dc9145b76c7ed58ddb7f
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-22 16:43:19 +0500

    destructoring

---
+++ javascript/day-8/10destuructoring.js
@@ -0,0 +1,7 @@
+let names = ["Oman" , "Turkey" , "Namabia" , "Cambodia" , "Zambia" , "Venenzuela"];
+
+let  [first , runner_up , ...rest] = names;
+
+console.log(first);
+console.log(runner_up);
+console.log(rest);
