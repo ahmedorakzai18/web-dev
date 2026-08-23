3f802fe5fa7897f4c8be5ee84a12898e207cf222
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-23 22:09:40 +0500

    DOM Navigation

---
+++ javascript/day-9-dom/9navigation.js
@@ -0,0 +1,9 @@
+let heading4 = document.querySelector("h4");
+
+console.log(heading4.parentElement);    // This will give the parent element of the h4 tag
+
+let parent = document.querySelector("div");
+
+console.log(parent.children); // This will give all the children of the div tag
+
+console.log(parent.childElementCount); // This will give the count of children of the div tag
