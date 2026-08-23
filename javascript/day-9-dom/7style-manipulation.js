03b48ec298e7f2c1e4980d03b37a9ed782a0cadf
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-23 17:30:52 +0500

    style manipulation

---
+++ javascript/day-9-dom/7style-manipulation.js
@@ -0,0 +1,20 @@
+let styleManip = document.querySelector("#heading1");
+styleManip.style.backgroundColor = "Black";
+
+//---------------------------------------------------
+
+let stylebody = document.querySelector("body");
+stylebody.style.backgroundColor = "Beige";
+
+//---------------------------------------------------
+
+let parastyle = document.querySelectorAll("p")[1];
+parastyle.style.color= "RoyalBlue";
+parastyle.style.textAlign= "center";
+
+//---------------------------------------------------
+
+let comicImage = document.querySelector("img");
+comicImage.style.display = "block";
+comicImage.style.marginLeft = "auto";
+comicImage.style.marginRight = "auto";
