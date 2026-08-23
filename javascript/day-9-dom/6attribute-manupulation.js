c995cc25974be70d31438158c4161a7f576525ce
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-23 16:53:10 +0500

    Get/Set Attribute manipulations

---
+++ javascript/day-9-dom/6attribute-manupulation.js
@@ -0,0 +1,27 @@
+//=================================================
+//-------------------------------------------------
+//=================================================
+
+
+// let change = document.querySelector("h1");
+
+// change.innerText = "Attribute Manipulation";
+
+// let link = document.querySelector("#a1");
+
+// link.href = "https://github.com/ahmedorakzai18";
+
+// let photo = document.querySelector("#pic1");
+
+// photo.src = "black.jpg"
+
+
+//=================================================
+//-------------------------------------------------
+//=================================================
+
+let image = document.getElementById("pic1");
+console.log(image);
+
+image.setAttribute("src" , "black.jpg");
+console.log(image);
