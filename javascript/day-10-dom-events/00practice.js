9aaabd6f84362f9204dc82247fe7095cd6c43564
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-24 15:31:10 +0500

    practicing

--- javascript/day-10-dom-events/00practice.js
+++ javascript/day-10-dom-events/00practice.js
@@ -3,6 +3,6 @@ let body = document.querySelector("body");
 let para1 = document.querySelector("#p1");
 let button = document.querySelector("#b1");
 
-button.addEventListener("click" , function(){
-    para1.innerText = "CHANGED";
+button.addEventListener("click", function () {
+  para1.innerText = "CHANGED";
 });
