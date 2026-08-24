b863849273444c6fbad7ddee9cdfde59184008a7
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-24 13:09:41 +0500

    addEventListener

---
+++ javascript/day-10-dom-events/01dom-events.js
@@ -0,0 +1,37 @@
+//-------------------SELECTING BODY=====================
+
+let body = document.querySelector("body");
+
+//=====================GIVING EVENT TO BUTTON: 1=====================
+
+let button = document.querySelector("#c1");
+
+button.addEventListener("click" , function(){
+    console.log("I was clicked to add the styling to the Document.");
+
+    body.style.backgroundColor = "pink";
+    body.style.border = "2px solid black";
+    body.style.fontFamily = "Arial";
+});
+
+//=====================GIVING EVENT TO BUTTON: 2=====================
+let button2 = document.querySelector("#c2");
+
+button2.addEventListener("click" , function(){
+       console.log("I was clicked to REMOVE the styling to the Document.");
+
+    body.style.backgroundColor = "";
+
+    body.style.fontFamily = "";
+});
+
+//=====================GIVING EVENT TO BUTTON: 3=====================
+let button3 = document.querySelector("#c3");
+
+button3.addEventListener("click" , function(){
+       console.log("I was clicked to REMOVE the styling to the Document.");
+
+    body.style.backgroundColor = "#00113a";
+    body.style.color = "#f8f2bf"
+    body.style.fontFamily = "Georgia";
+});
