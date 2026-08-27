9f4815d5ca93f35e5345ecc1d6eb19a139594b3e
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-27 06:32:16 +0500

    Simon Say's Game Completed

--- javascript/day-11/game/simon-says.js
+++ javascript/day-11/game/simon-says.js
@@ -31,7 +31,7 @@
  btn.classList.add("userflash");
  setTimeout(function(){
  btn.classList.remove("userflash");
-} , 1000);
+} , 100 );
  }
 
 function levelUp() { 
@@ -41,24 +41,48 @@ function levelUp() {
 
     let randIdx = Math.floor(Math.random() * 4); 
     let randColor = btnColors[randIdx]; 
-    gameSeq.push(randColor);
     let randbtn = document.querySelector(`.${randColor}`); 
-
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randbtn);
-
+    gameSeq.push(randColor);
+    console.log(gameSeq);
     gameflash(randbtn);
 }
 
-function btnPress(){
-    let btn  =  this;
-    userFlash(btn);
-    let userColor = btn.getAttribute("id");
-    userSeq.push(userColor);
+function checkAns(idx) {
+if (userSeq[idx] === gameSeq[idx]) {
+if (userSeq. length == gameSeq. length){
+setTimeout(levelUp, 1000);
+}
+} else {
+h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start.`; 
+document.querySelector("body").style.backgroundColor = "red"; 
+setTimeout(function(){
+document.querySelector("body").style.backgroudColor = "white";
+}, 150);
+
+resetGame(); 
+}
 }
 
-let allBtns = document.querySelectorAll(".btn"); 
+
+function btnPress(){ 
+    let btn = this; 
+    userFlash(btn); 
+    
+    userColor = btn.getAttribute("id"); 
+    userSeq.push(userColor); 
+    
+
+    checkAns(userSeq.length - 1); 
+} 
+
+let allBtns = document.querySelectorAll(".box"); 
 for(let btn of allBtns){
     btn.addEventListener("click", btnPress); 
 } 
+
+function resetGame() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
