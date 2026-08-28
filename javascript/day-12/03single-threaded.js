a4a22bc9d2dd47547e17b90a949e7e9e9b2860b0
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-08-28 16:59:15 +0500

    Single Threded Concept

---
+++ javascript/day-12/03single-threaded.js
@@ -0,0 +1,19 @@
+//===================Synchrous Nature===================
+
+// let a = 10; 
+// console.log(a);
+
+// let b = 38; 
+// console.log(b);
+
+// console.log(a+b);
+
+
+//===================Asynchrous Nature===================
+
+
+setTimeout(function(){
+    console.log("Hello EveryOne");
+} , 3000);
+
+console.log("hey");
